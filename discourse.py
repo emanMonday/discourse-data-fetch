5fe87085eda39b064bdeaad86b52e04f71908044
emanMonday <57182886+emanMonday@users.noreply.github.com>
2021-03-24 09:51:12 +0200

    add email fetching

--- discourse.py
+++ discourse.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 18 17:17:08 2021
 
-@author: emanmarcu
-"""
-
-import requests 
+import requests
 import pandas as pd
-
-# Settings
-APIKEY="API_KEY"
-APIUSERNAME="USERNAME"
-QSPARAMS = {"api_key": APIKEY, "api_username": APIUSERNAME}
+import time
+# =============================================================================
+# api settings
+# =============================================================================
+APIKEY = '7f93f8db63ff36e361e8760a8c95322bdb9f4d5ea694ad578e866607802ba19e'
+USERNAME = 'Becca-monday.com'
+HEADERS = {"Api-Key": APIKEY, "Api-Username": USERNAME}
+sleep_secs = 0.3
 
 # =============================================================================
 # categories
 # =============================================================================
 FORUM = "https://community.monday.com/categories.json"
-r = requests.get(FORUM, params=QSPARAMS)
+r = requests.get(FORUM, headers=HEADERS)
 categories = r.json()['category_list']['categories']
 cats = []
 for category in categories:
@@ -39,7 +37,7 @@ for pair in id_name_pairs:
         FORUM = FORUM.replace('ID', id )
         FORUM = FORUM.replace('SLUG', slug)
         FORUM = FORUM.replace('PAGE', str(j))
-        r = requests.get(FORUM, params=QSPARAMS)
+        r = requests.get(FORUM, headers=HEADERS)
         rj = r.json()
         
         if rj['topic_list']['topics'] == []:
@@ -50,6 +48,8 @@ for pair in id_name_pairs:
         df_topics_cat['category_slug'] = slug
         df_topics = pd.concat([df_topics, df_topics_cat])
         
+        time.sleep(sleep_secs)
+        
 # =============================================================================
 # posts
 # =============================================================================
@@ -59,19 +59,60 @@ for pair in id_slug_pairs:
     id = str(pair[0])
     FORUM = "https://community.monday.com/t/ID.json"
     FORUM = FORUM.replace('ID', id )
-    r = requests.get(FORUM, params=QSPARAMS)
+    r = requests.get(FORUM, headers=HEADERS)
     rj = r.json()
     df_posts_topic = pd.DataFrame(rj['post_stream']['posts'])
     df_posts = pd.concat([df_posts, df_posts_topic])
 
+    time.sleep(sleep_secs)
 
 # =============================================================================
 # tags
 # =============================================================================
 FORUM = "https://community.monday.com/tags.json"
-r = requests.get(FORUM, params=QSPARAMS)
+r = requests.get(FORUM, headers=HEADERS)
 rj = r.json()
 df_tags = pd.DataFrame(rj['tags'])
+
+
+# =============================================================================
+# users
+# =============================================================================
+user_types = ['active', 'new', 'staff', 'suspended', 'blocked', 'suspect']
+df_users = pd.DataFrame()
+for user_type in user_types:
+
+    for j in range(10000000):   
+        FORUM = "https://community.monday.com/admin/users/list/USERTYPE.json?ascending=false&page=PAGE"
+        FORUM = FORUM.replace('USERTYPE', user_type)
+        FORUM = FORUM.replace('PAGE', str(j))
+        r = requests.get(FORUM, headers=HEADERS)
+        rj = r.json()
+
+        df_users_type = pd.DataFrame(rj)
+        df_users_type['user_type'] = user_type
+        df_users = pd.concat([df_users, df_users_type])
+        
+        time.sleep(sleep_secs)
+        if rj == []:
+            break        
+        
+df_users = df_users.drop(['email', 'secondary_emails'], axis=1)
+        
+user_names =  df_users.username.drop_duplicates().values
+emails_dict = {}
+for user_name in user_names:
+    FORUM = "https://community.monday.com/u/USERNAME/emails.json"
+    FORUM = FORUM.replace('USERNAME', user_name)
+    r = requests.get(FORUM, headers=HEADERS)
+    rj = r.json()
+    emails_dict[user_name] = rj
+
+emails = pd.DataFrame(emails_dict).T
+emails.index.name = 'username'
+emails = emails.reset_index()
+    
+df_users = pd.merge(df_users, emails, on='username', how='left')
 
 
 # Dump the following tables into BB DB:
@@ -79,3 +120,4 @@ df_tags = pd.DataFrame(rj['tags'])
 #df_topics
 #df_posts
 #df_tags
+#df_users
