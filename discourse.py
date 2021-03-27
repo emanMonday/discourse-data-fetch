f9797a349a76b2721e35972b88e11e1d0cc11c30
emanMonday <57182886+emanMonday@users.noreply.github.com>
2021-03-27 15:18:32 +0300

    refactored to fetch via explorer queries
    instead of direct via API

--- discourse.py
+++ discourse.py
@@ -1,123 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import requests
-import pandas as pd
-import time
-# =============================================================================
-# api settings
-# =============================================================================
-APIKEY = 'APIKEY'
-USERNAME = 'USERNAME'
-HEADERS = {"Api-Key": APIKEY, "Api-Username": USERNAME}
-sleep_secs = 0.3
+import json
+import pandas as pd 
+import numpy as np
+from functions import get_query, run_sequential_queries
 
-# =============================================================================
-# categories
-# =============================================================================
-FORUM = "https://community.monday.com/categories.json"
-r = requests.get(FORUM, headers=HEADERS)
-categories = r.json()['category_list']['categories']
-cats = []
-for category in categories:
-    cats.append(category)
-df_cats = pd.DataFrame(cats)
+types = ['topics', 
+         'posts', 
+         'topic_tags', 
+         'users', 
+         'user_actions', 
+         'user_badges', 
+         'tags', 
+         'badges', 
+         'categories'
+        ]
 
-# =============================================================================
-# topics
-# =============================================================================
-id_name_pairs = tuple(zip(df_cats['id'].values, df_cats['slug'].values))
-df_topics = pd.DataFrame()
-for pair in id_name_pairs:
-    id = str(pair[0])
-    slug = str(pair[1])
-    
-    for j in range(10000000):    
-        FORUM = "https://community.monday.com/c/SLUG/ID/l/latest.json?ascending=false&page=PAGE"
-        FORUM = FORUM.replace('ID', id )
-        FORUM = FORUM.replace('SLUG', slug)
-        FORUM = FORUM.replace('PAGE', str(j))
-        r = requests.get(FORUM, headers=HEADERS)
-        rj = r.json()
-        
-        if rj['topic_list']['topics'] == []:
-            break
-            
-        df_topics_cat = pd.DataFrame(rj['topic_list']['topics'])
-        df_topics_cat['category_id'] = id
-        df_topics_cat['category_slug'] = slug
-        df_topics = pd.concat([df_topics, df_topics_cat])
-        
-        time.sleep(sleep_secs)
-        
-# =============================================================================
-# posts
-# =============================================================================
-id_slug_pairs = tuple(zip(df_topics['id'].values, df_topics['slug'].values))
-df_posts = pd.DataFrame()
-for pair in id_slug_pairs:
-    id = str(pair[0])
-    FORUM = "https://community.monday.com/t/ID.json"
-    FORUM = FORUM.replace('ID', id )
-    r = requests.get(FORUM, headers=HEADERS)
-    rj = r.json()
-    df_posts_topic = pd.DataFrame(rj['post_stream']['posts'])
-    df_posts = pd.concat([df_posts, df_posts_topic])
-
-    time.sleep(sleep_secs)
-
-# =============================================================================
-# tags
-# =============================================================================
-FORUM = "https://community.monday.com/tags.json"
-r = requests.get(FORUM, headers=HEADERS)
-rj = r.json()
-df_tags = pd.DataFrame(rj['tags'])
-
-
-# =============================================================================
-# users
-# =============================================================================
-user_types = ['active', 'new', 'staff', 'suspended', 'blocked', 'suspect']
-df_users = pd.DataFrame()
-for user_type in user_types:
-
-    for j in range(10000000):   
-        FORUM = "https://community.monday.com/admin/users/list/USERTYPE.json?ascending=false&page=PAGE"
-        FORUM = FORUM.replace('USERTYPE', user_type)
-        FORUM = FORUM.replace('PAGE', str(j))
-        r = requests.get(FORUM, headers=HEADERS)
-        rj = r.json()
-
-        df_users_type = pd.DataFrame(rj)
-        df_users_type['user_type'] = user_type
-        df_users = pd.concat([df_users, df_users_type])
-        
-        time.sleep(sleep_secs)
-        if rj == []:
-            break        
-        
-df_users = df_users.drop(['email', 'secondary_emails'], axis=1)
-        
-user_names =  df_users.username.drop_duplicates().values
-emails_dict = {}
-for user_name in user_names:
-    FORUM = "https://community.monday.com/u/USERNAME/emails.json"
-    FORUM = FORUM.replace('USERNAME', user_name)
-    r = requests.get(FORUM, headers=HEADERS)
-    rj = r.json()
-    emails_dict[user_name] = rj
-
-emails = pd.DataFrame(emails_dict).T
-emails.index.name = 'username'
-emails = emails.reset_index()
-    
-df_users = pd.merge(df_users, emails, on='username', how='left')
-
-
-# Dump the following tables into BB DB:
-#df_cats
-#df_topics
-#df_posts
-#df_tags
-#df_users
+dfs = {}
+for typ in types:
+    query = get_query(typ)
+    dfs[typ] = run_sequential_queries(query, np, pd, json, requests)
---
+++ functions.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def get_query(typ):
+    
+    queries = {}
+    
+    queries['topics'] = '''
+            SELECT t.*
+            FROM topics t
+            join posts p
+            on t.id = p.topic_id 
+            and p.post_number = 1
+            and p.user_id > 0
+            order by t.created_at
+            '''
+    
+    queries['posts'] = '''
+            with _topics as (
+                SELECT distinct t.id as topic_id
+                FROM topics t
+                join posts p
+                on t.id = p.topic_id 
+                and p.post_number = 1
+                and p.user_id > 0
+                )
+            
+            select p.*
+            from posts p
+            join _topics t
+                on t.topic_id = p.topic_id 
+                and p.user_id > 0
+            order by p.id
+            '''
+    
+    queries['topic_tags'] = '''
+            with _topics as (
+                SELECT distinct t.id as topic_id
+                FROM topics t
+                join posts p
+                on t.id = p.topic_id 
+                and p.post_number = 1
+                and p.user_id > 0
+                )
+            select tt.*
+            from topic_tags tt
+            join _topics t
+                on t.topic_id = tt.topic_id 
+            order by tt.id
+            '''
+    
+    queries['users'] = '''
+            select ue.email 
+                    , u.*
+            from users u
+            join user_emails ue
+                on u.id = ue.user_id
+                and u.id > 0
+            order by u.id
+            '''
+    
+    queries['user_badges'] = '''
+            select *
+            from user_badges
+            where user_id > 0
+            order by id
+            '''
+        
+    queries['user_actions'] = '''
+            select *
+            from user_actions
+            where user_id > 0
+            order by id
+            '''
+            
+    queries['badges'] = '''
+            select *
+            from badges
+            order by id
+            '''
+            
+    queries['tags'] = '''
+            select *
+            from tags
+            order by id
+            '''
+    
+    queries['categories'] = '''
+            select *
+            from categories
+            order by id
+            '''
+        
+    return queries[typ]
+
+def run_sequential_queries(query, np, pd, json, requests):
+ 
+    API_KEY = '7f93f8db63ff36e361e8760a8c95322bdb9f4d5ea694ad578e866607802ba19e'
+    API_USERNAME = 'Becca-monday.com'
+    headers = {'Content-Type': 'multipart/form-data', 'Api-Key': API_KEY, 'Api-Username': API_USERNAME}
+
+    query += ' limit 1000 offset OFFSET'
+    rang = np.arange(0,10000000000, 1000)
+    df = pd.DataFrame()
+    
+    for offset in rang:
+        # UPDATE
+        ENDPOINT = "https://community.monday.com/admin/plugins/explorer/queries/28"
+        query_to_request = query.replace('OFFSET', str(int(offset)))
+        request = requests.put(ENDPOINT, 
+                               headers=headers, 
+                               data="query[sql]="+query_to_request) # results in  <Response [400]>
+        # RUN
+        ENDPOINT = "https://community.monday.com/admin/plugins/explorer/queries/28/run"
+        request = requests.post(ENDPOINT, headers=headers)
+        data = request.content
+        x = json.loads(data.decode())
+        df_offset = pd.DataFrame(x['rows'], columns=x['columns'])
+        if df_offset.shape[0] == 0:
+            break
+        df = pd.concat([df, df_offset])
+    return df
